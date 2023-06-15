8b246fb823dde8252e742f50c61cc89e1c249b22
Sirius018 <betochirakudai@gmail.com>
2023-06-15 10:10:20 -0500

    cambios

---
+++ src/main/java/com/inventorygenius/repository/ICategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.inventorygenius.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICategoriaRepository extends CrudRepository<com.inventorygenius.model.Categoria, Integer>{
+
+}
---
+++ src/main/java/com/inventorygenius/repository/IPaisRepository.java
@@ -0,0 +1,7 @@
+package com.inventorygenius.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IPaisRepository extends CrudRepository<com.inventorygenius.model.Pais, Integer>{
+
+}
