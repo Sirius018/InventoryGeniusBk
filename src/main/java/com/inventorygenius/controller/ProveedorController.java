0085dc2e7672341e8e0ea51c83845732f5be5bc0
Sirius018 <betochirakudai@gmail.com>
2023-06-16 22:37:28 -0500

    insert

--- src/main/java/com/inventorygenius/controller/ProveedorController.java
+++ src/main/java/com/inventorygenius/controller/ProveedorController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.inventorygenius.model.Proveedor;
 import com.inventorygenius.repository.IProveedorRepository;
 
 @Controller
@@ -12,6 +15,26 @@ public class ProveedorController {
 
 	@Autowired
 	private IProveedorRepository repoProv;
+	
+	
+	
+	@PostMapping("/proveedor/guardar")
+	public String grabarCrudProducto(@ModelAttribute Proveedor proveedor, Model model) {
+		System.out.println(proveedor);
+		try {
+			model.addAttribute("LstProductos", repoProv.findAll());
+			repoProv.save(proveedor);
+			model.addAttribute("mensaje","Registro OK");
+			System.out.println(proveedor);
+			
+		} catch (Exception e) {
+			model.addAttribute("mensaje","Error al Registrar");
+		}
+		model.addAttribute("btnRegistrarProveedor", "Registrar");
+		
+		return "Proveedor";
+	}
+	
 	
 	@GetMapping("/listadoProveedores")
 	public String listarProveedor(Model model) {
